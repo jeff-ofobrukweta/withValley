e67014e7ba0b5bb71d92b178315d22f5836ca8d0
jeff-ofobrukweta <oghenerukevwejeff@gmail.com>
2025-07-28 01:57:12 +0100

    chore: added stubbed data

--- src/components/profile-card/index.tsx
+++ src/components/profile-card/index.tsx
@@ -13,54 +13,6 @@ import AccordionSectionList from "./AccordionSectionList";
 import ProfileFooter from "./ProfileFooter";
 
 export default function ProfileCard() {
-  // const tags = [
-  //   { label: "Stop Outreach", icon: Hand },
-  //   { label: "Meeting Scheduled", icon: Calendar },
-  //   { label: "Human Handover", icon: Heart },
-  //   { label: "Interested", icon: Flame },
-  // ];
-
-  // const sections = [
-  //   { title: "Intent Data", icon: ScanEye },
-  //   { title: "Experience", icon: Crown },
-  //   { title: "Education", icon: GraduationCap },
-  //   { title: "LinkedIn Bio", icon: Linkedin },
-  // ];
-
-  // const contactInfo = [
-  //   {
-  //     icon: <Lock className="w-4 h-4" />,
-  //     label: "Privacy",
-  //     value: <span className="font-medium text-gray-900">Public Profile</span>,
-  //   },
-  //   {
-  //     icon: <Linkedin className="w-4 h-4" />,
-  //     label: "Linkedin",
-  //     value: (
-  //       <span className="bg-gray-100 text-gray-900 rounded-full px-3 py-1 font-medium max-w-full truncate inline-block">
-  //         linkedin.com/company/qo...
-  //       </span>
-  //     ),
-  //   },
-  //   {
-  //     icon: <Mail className="w-4 h-4" />,
-  //     label: "Email",
-  //     value: (
-  //       <span className="bg-gray-100 text-gray-900 rounded-full px-3 py-1 font-medium max-w-full truncate inline-block">
-  //         skeshav628@gmail.com
-  //       </span>
-  //     ),
-  //   },
-  //   {
-  //     icon: <MapPin className="w-4 h-4" />,
-  //     label: "Address",
-  //     value: (
-  //       <span className="font-medium text-gray-900 max-w-full truncate inline-block">
-  //         California, United States of A...
-  //       </span>
-  //     ),
-  //   },
-  // ];
 
   return (
     <div className="max-w-sm mx-auto">
